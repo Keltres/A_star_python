13ecdb33cb70cfce97fc37eb252cdff5bd3d22b9
Keltres <pcichon42@gmail.com>
2021-01-11 21:49:42 +0000

    astar_search not yet in use

--- astar_search.py
+++ astar_search.py
@@ -1,3 +1,5 @@
+# IMPORTANT not yet in use
+
 class astarSearch():
     """Class responsible for executing a* algorithm on a piece matrix of data"""
     self.matrix = []        #a list of all nodes
